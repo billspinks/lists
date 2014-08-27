910f61a4f6298ed6895c21479e4d48c7bedce21f
Bill Spinks <bill.spinks@gmail.com>
2014-08-27 11:44:14 -0500

    Initial commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+__author__ = 'bill'
+list = ['abcd',786,2.23,'john',70.2]
+tinylist = [123,'john']
+
+print list
+print list[0]
+print list[1:3]
+print list * 2
+print tinylist
+
+print list + tinylist
+
+# this guy 70.20000000000000003  why do I get only one decimal place
